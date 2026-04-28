5cfec3105fe8c9800e9214e58b587c685b64594e
gremlin <gremlincache@gmail.com>
2026-04-28 09:15:35 +0200

    wavecrest chests added

--- itemIndex.js
+++ itemIndex.js
@@ -11,14 +11,14 @@
 
     window.itemIndex = {
 // FEST CHESTS
-67966: { name: "Evergreen Expanse Chest", category: "Other", element: ["wind", "neutral"], allVenues: true, festival: "elemental"},
-67967: { name: "Gale Winds Chest", category: "Other", element: ["wind", "neutral"], allVenues: true, festival: "elemental"},
-67968: { name: "Lucky Feather Chest", category: "Other", element: ["wind", "neutral"], allVenues: true, festival: "elemental"},
-67969: { name: "The Double of the Wind Chest", category: "Other", element: ["wind", "neutral"], allVenues: true, festival: "elemental"},
-67970: { name: "Daffodil Dance Chest", category: "Other", element: ["wind", "neutral"], allVenues: true, festival: "elemental"},
-67971: { name: "Really Winging It Chest", category: "Other", element: ["wind", "neutral"], allVenues: true, festival: "elemental"},
-67972: { name: "Zephyrous Zenith Chest", category: "Other", element: ["wind", "neutral"], allVenues: true, festival: "elemental"},
-67973: { name: "Windbound Spirit Chest", category: "Other", element: ["wind", "neutral"], allVenues: true, festival: "elemental"},
+68304: { name: "Gone with the Flow Chest", category: "Other", element: ["water", "neutral"], allVenues: true, festival: "elemental"},
+68305: { name: "Abandoned, Adrift Chest", category: "Other", element: ["water", "neutral"], allVenues: true, festival: "elemental"},
+68306: { name: "Fishy Friends Chest", category: "Other", element: ["water", "neutral"], allVenues: true, festival: "elemental"},
+68307: { name: "Esturary Nursery Chest", category: "Other", element: ["water", "neutral"], allVenues: true, festival: "elemental"},
+68308: { name: "Thresher Hunt Chest", category: "Other", element: ["water", "neutral"], allVenues: true, festival: "elemental"},
+68309: { name: "Jewel of the Deep Chest", category: "Other", element: ["water", "neutral"], allVenues: true, festival: "elemental"},
+68310: { name: "School of Thought Chest", category: "Other", element: ["water", "neutral"], allVenues: true, festival: "elemental"},
+68311: { name: "Cirrocampi Chest", category: "Other", element: ["water", "neutral"], allVenues: true, festival: "elemental"},
 
 // APPAREL
 268: { name: "Grey Wizard Hat", category: "Apparel", enemies: ["Wandering Surge Priest"] },
