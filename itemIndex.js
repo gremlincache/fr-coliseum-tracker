c1cd6fb0a36f8115782797dcce368621e1fc27bf
gremlin <gremlincache@gmail.com>
2026-09-21 18:36:12 +0200

    Starfall celebration chests

--- itemIndex.js
+++ itemIndex.js
@@ -11,14 +11,14 @@
 
     window.itemIndex = {
 // FEST CHESTS
-69716: { name: "Magma Cat Chest", category: "Other", element: ["fire", "neutral"], allVenues: true, festival: "elemental"},
-69717: { name: "Furnace Delver Chest", category: "Other", element: ["fire", "neutral"], allVenues: true, festival: "elemental"},
-69718: { name: "Flamelapse Chest", category: "Other", element: ["fire", "neutral"], allVenues: true, festival: "elemental"},
-69719: { name: "Song of Ashes Chest", category: "Other", element: ["fire", "neutral"], allVenues: true, festival: "elemental"},
-69720: { name: "Fire Dance Chest", category: "Other", element: ["fire", "neutral"], allVenues: true, festival: "elemental"},
-69721: { name: "Flame-Forged Hero Chest", category: "Other", element: ["fire", "neutral"], allVenues: true, festival: "elemental"},
-69722: { name: "Set Blue Sea Ablaze Chest", category: "Other", element: ["fire", "neutral"], allVenues: true, festival: "elemental"},
-69723: { name: "Fan the Flame Chest", category: "Other", element: ["fire", "neutral"], allVenues: true, festival: "elemental"},
+70070: { name: "Arcane Ephemeris Chest", category: "Other", element: ["arcane", "neutral"], allVenues: true, festival: "elemental"},
+70071: { name: "Cuffs of Constellation Chest", category: "Other", element: ["arcane", "neutral"], allVenues: true, festival: "elemental"},
+70072: { name: "Cosmic Anomaly Chest", category: "Other", element: ["arcane", "neutral"], allVenues: true, festival: "elemental"},
+70073: { name: "Starsoup Chest", category: "Other", element: ["arcane", "neutral"], allVenues: true, festival: "elemental"},
+70074: { name: "Loopholes Chest", category: "Other", element: ["arcane", "neutral"], allVenues: true, festival: "elemental"},
+70075: { name: "Arcanosynthesis Chest", category: "Other", element: ["arcane", "neutral"], allVenues: true, festival: "elemental"},
+70076: { name: "Seeping Runes Chest", category: "Other", element: ["arcane", "neutral"], allVenues: true, festival: "elemental"},
+70077: { name: "Crystal Binder Chest", category: "Other", element: ["arcane", "neutral"], allVenues: true, festival: "elemental"},
 
 // APPAREL
 268: { name: "Grey Wizard Hat", category: "Apparel", enemies: ["Wandering Surge Priest"] },
